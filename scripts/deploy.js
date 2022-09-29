cbabb20a3f42455859611a0db1d4b6af48679c42
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-09-29 12:11:46 -0300

    commented sample deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
+/* // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
@@ -29,3 +29,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+ */
